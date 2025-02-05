77ca3fae813a9aed005d2e46ad6a66db8c1a1f34
liamrichardson <liampgrichardson@gmail.com>
2025-02-05 17:07:23 +0000

    addressed errors in deploy tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -168,5 +168,5 @@ output "ec2_public_ip" {
 output "ec2_instance_id" {
   description = "The ID of the EC2 instance"
   value       = aws_instance.my_ec2.id
-  sensitive   = false
+  sensitive   = true
 }
