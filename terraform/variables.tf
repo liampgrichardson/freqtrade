cc366ebc713966c76a95f8567a7d4da60a4a8af8
liamrichardson <liampgrichardson@gmail.com>
2025-02-05 00:08:22 +0000

    made changes to variables.tf and destroy and deploy yml files for different variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,11 +4,6 @@ variable "aws_region" {
   type        = string
 }
 
-variable "ecr_repository_name" {
-  description = "Name of the ECR repository, gotten from gh actions"
-  type        = string
-}
-
 # Global tags
 variable "global_tags" {
   description = "A map of global tags to apply to all resources"
@@ -17,10 +12,5 @@ variable "global_tags" {
 
 variable "ec2_ssh_public_key" {
   description = "Public SSH key for EC2 access"
-  type        = string
-}
-
-variable "image_tag" {
-  description = "The Docker image tag to use for the ECS task definition"
   type        = string
 }
